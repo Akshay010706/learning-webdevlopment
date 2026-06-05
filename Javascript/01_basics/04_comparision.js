a0c7fd47905f08f4c1220a069f7a99615313937d
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-06 04:53:32 +0530

    comparison

---
+++ Javascript/01_basics/04_comparision.js
@@ -0,0 +1,37 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2" > 1); //true
+console.log("02" > 1); //true
+
+//both should have save data to compare 
+//but javascript changes on its own
+
+console.log(null>0); //false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+
+// avoid these type of conversion
+
+
+// ===     strict check checks value and the data type both 
+
+console.log("2"===2);
+
+
+
+
+
+
+
+
