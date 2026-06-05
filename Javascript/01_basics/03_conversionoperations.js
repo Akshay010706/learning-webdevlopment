e5e2da04d81f2bae297b5e8092eb98e2c7c4e1c0
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-06 04:41:48 +0530

    operations javascript

--- Javascript/01_basics/03_conversionoperations.js
+++ Javascript/01_basics/03_conversionoperations.js
@@ -28,3 +28,46 @@ console.log(typeof stringnumber);
 
 
 
+// ************************** Operations ************************ //
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Akshay"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1"+"2")//12
+// console.log("1"+2+2); // 122
+// console.log(1+2+"2"); //32
+
+
+console.log(+true);//1
+console.log(+"");//0
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++; //++gamecounter  -- prefix and post fix operator js mdn read the description
+console.log(gamecounter);
+
+
+
+
+
+
