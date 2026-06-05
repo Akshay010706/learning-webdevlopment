7c8ed3170eea6ff61e78e31d6abd79e1ba44a049
Akshay pratap singh <akshaypratapsingh529@gmail.com>
2026-06-05 17:47:32 +0000

    data types

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)//we are using nodejs, not browser console
+
+//console.log(3+3); console.log("akshay")
+//code rediability is not good
+
+let name ="akshay"
+let age = 18 
+let isloggedin = false
+let state; //undefined
+letstate=null //empty , standalone value
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standalone value
+//undefined =>
+//symbol => unique
+
+// object
+
+console.log(typeof "akshay" )
+console.log(typeof name )
+console.log(typeof undefined )
+console.log(typeof null )
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const account_Id = 17829
+let accountEmail = "akshay@gmail.com" 
+var accountpassword = "12345" //var has scope problem
+accountcity = "jaipur"
+
+let accountstate;//value is undifined if given non
+
+//account_Id = 2 //not allowed
+
+accountEmail ="akssh@gmail.com"
+accountpassword = "kanpur"
+console.log("account_Id");
+
+console.table([account_Id,accountcity,accountEmail,accountpassword])
+
+/*
+prefer not to use var because 
+of block scope and functional scope
+let is used mostelt 
+const is used to constant
+*/
