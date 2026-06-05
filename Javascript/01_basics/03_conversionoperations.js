bb56b4d3ae02ed38f6937005def324d364cae662
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-06 04:20:59 +0530

    conversion of datatypes

---
+++ Javascript/01_basics/03_conversionoperations.js
@@ -0,0 +1,30 @@
+let score = "33abc" //33 or null or undifidend , . string
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);  
+
+//"33" =>33
+//"33abc" => NaN
+//true => 1 , false => 0
+//null => 0
+
+let isloogedin = 1 //"","akshay"
+
+let booleanisloggedin = Boolean(isloogedin)
+console.log(booleanisloggedin);
+
+//1 => true , 0=>false
+// "" => false
+//"akshay" => true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
